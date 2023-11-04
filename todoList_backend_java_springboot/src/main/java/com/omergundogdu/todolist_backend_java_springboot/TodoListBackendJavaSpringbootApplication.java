568b0a173fdd35651b867081570988e003a8d5d1
ÖMER GÜNDOĞDU <omergundogdu75@gmail.com>
2023-11-04 11:37:29 +0300

    spring backend project config

---
+++ todoList_backend_java_springboot/src/main/java/com/omergundogdu/todolist_backend_java_springboot/TodoListBackendJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.omergundogdu.todolist_backend_java_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListBackendJavaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListBackendJavaSpringbootApplication.class, args);
+    }
+
+}
---
+++ todoList_backend_java_springboot/src/test/java/com/omergundogdu/todolist_backend_java_springboot/TodoListBackendJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omergundogdu.todolist_backend_java_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListBackendJavaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
